96fb6c4730c31c54aeab4a2be1a1df19bf550b52
akuthalingam3 <annapooranik@gatech.edu>
2021-01-15 15:30:10 -0800

    first commit

---
+++ chapter2/exercise_2_1_simple_message.py
@@ -0,0 +1,4 @@
+# 2-1. Simple Message: Assign a message to a variable , and then print that message
+
+message="Hi! This is a simple message for the exercise in the chapter 2 of python crash course book."
+print(message)
