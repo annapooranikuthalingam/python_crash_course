0079cd10bbd724f7c27ecc3ca06c79c92da05001
akuthalingam3 <annapooranik@gatech.edu>
2021-01-15 17:40:33 -0800

    initial status

---
+++ chapter3/exercise_3_2_greetings.py
@@ -0,0 +1,10 @@
+"""
+3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name,
+print a message to them. The text of each message should be the same, but each message should be personalized
+with the person’s name.
+"""
+names=['friend1','friend2','friend3','friend4']
+print(f'Good morning {names[0]}')
+print(f'Good morning {names[1]}')
+print(f'Good morning {names[2]}')
+print(f'Good morning {names[3]}')
